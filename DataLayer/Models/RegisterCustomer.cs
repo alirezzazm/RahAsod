9b751bdc79043625c887dce9b9d829aaba351fae
Aria <ariadarvish99@gmail.com>
2020-12-03 23:44:54 +0330

    1399/08/13 vs 7 Aria thirdparty & registration

--- DataLayer/Models/RegisterCustomer.cs
+++ DataLayer/Models/RegisterCustomer.cs
@@ -5,17 +5,42 @@ using System.Text;
 
 namespace DataLayer.Models
 {
-   public class RegisterCustomer
+    public class RegisterCustomer
     {
         [Key]
         public int CustomerID { get; set; }
-        public string Name { get; set; }
-        public string Family { get; set; }
-        public string NumberPhone { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "نام خانوادگي")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string LastName { get; set; }
+
+        [Display(Name = "کد ملي")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
         public string NationalCode { get; set; }
-        public string Adrress { get; set; }
-        public string Email { get; set; }
+
+        [Display(Name = "شماره همراه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "تلفن")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
         public string HomeNumber { get; set; }
+
+        [Display(Name = "ايميل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "آدرس")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string Adrress { get; set; }
+
+        [Display(Name = "")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
         public string InsurancesForCustomer { get; set; }
     }
 }
