6b6e6714fabda13c8a0b74606c94732122d215f7
Aria <ariadarvish99@gmail.com>
2020-12-08 21:40:57 +0330

    additional change

--- DataLayer/Models/RegisterCustomer.cs
+++ DataLayer/Models/RegisterCustomer.cs
@@ -11,6 +11,7 @@ namespace DataLayer.Models
         [Key]
         public int CustomerID { get; set; }
 
+
         [Display(Name = "نام")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
         public string FirstName { get; set; }
@@ -43,5 +44,29 @@ namespace DataLayer.Models
         [Display(Name = "")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
         public string InsurancesForCustomer { get; set; }
+
+        [Display(Name = "نام ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string NameCustomer { get; set; }
+        [Display(Name = "نام ونام خانوادگی ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string FamilyCustomer { get; set; }
+        [Display(Name = "شماره همراه  ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string NumberPhoneCustomer { get; set; }
+        [Display(Name = "کد ملی ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string NationalCodeCustomer { get; set; }
+        [Display(Name = "آدرس ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string AdrressCustomer { get; set; }
+        [Display(Name = "ایمیل ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string EmailCustomer { get; set; }
+        [Display(Name = "شماره منزل ")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنيد ")]
+        public string HomeNumberCustomer { get; set; }
+
+
     }
 }
