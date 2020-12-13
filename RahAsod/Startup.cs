2e22caaf5be6f19d2ce31fa2a9536a987b5515af
alireza <alireza@DESKTOP-JA7EG0A>
2020-12-13 18:44:42 +0330

    1399/09/23 vs 1

--- RahAsod/Startup.cs
+++ RahAsod/Startup.cs
@@ -34,7 +34,7 @@ namespace RahAsod
             services.AddRazorPages()
             .AddRazorPagesOptions(options =>
              {
-                 options.Conventions.AddPageRoute("/MainPage/HomePage", "");
+                 options.Conventions.AddPageRoute("/CustomerRegistrationPages/ForgotPasswordCustomerPage", "");
              });
         }
 
