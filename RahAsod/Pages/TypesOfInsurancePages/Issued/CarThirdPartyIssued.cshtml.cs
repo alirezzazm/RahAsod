8cf6cef246140a8a98577cdf2f51883b1a1bec40
alireza <alireza@DESKTOP-JA7EG0A>
2020-12-26 20:51:26 +0330

    1399/10/06 vs 1

--- RahAsod/Pages/TypesOfInsurancePages/Issued/CarThirdPartyIssued.cshtml.cs
+++ RahAsod/Pages/TypesOfInsurancePages/Issued/CarThirdPartyIssued.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataLayer.Models.InsurancesDescription;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,30 @@ namespace RahAsod.Pages.TypesOfInsurancePages.Issued
 {
     public class CarThirdPartyIssuedModel : PageModel
     {
-        public void OnGet()
+        private readonly DataLayer.Context.InsuranceContext _context;
+        //public CarThirdPartyInsuranceModel(DataLayer.Context.InsuranceContext context)
+        //{
+        //    _context = context;
+        //}
+        public IActionResult OnGet()
         {
+            return Page();
+        }
+
+        [BindProperty]
+        public DescriptionOfThirdPartyInsurance DescriptionOfThirdPartyInsurance { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.ThirdPartyInsurance.Add(DescriptionOfThirdPartyInsurance);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
